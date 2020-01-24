d12884953319f6f4bc10669762f2c0e22cffeedb
Andrea <andreaa.malin@gmail.com>
2020-01-24 16:49:57 -0600

    Codigo completo

--- app/src/main/java/com/example/fitapp/MainActivity.kt
+++ app/src/main/java/com/example/fitapp/MainActivity.kt
@@ -1,35 +1,32 @@
 package com.example.fitapp
 
-import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_running.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     //Initializing
-    private var actualLaps = 0
+    private var actualLaps = 0 //Actual lap
+    private var maxLaps = 20 //Max of laps
     private lateinit var buttonLaps: Button
     private lateinit var buttonRefresh: Button
-    private var lapsNum = 0
     lateinit var prizeImage: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_running)
+        setContentView(R.layout.activity_main)
         //Defining values
         buttonLaps = findViewById(R.id.buttonLaps)
         buttonRefresh = findViewById(R.id.buttonRefresh)
         prizeImage = findViewById(R.id.noBackground)
-        lapsNum = intent.getIntExtra("lapsNumber", 0)
 
         //Laps button
         buttonLaps.setOnClickListener {
             sumLap()
-            textLaps.text = actualLaps.toString()
-
+            showPrize()
         }
         //Refresh button
         buttonRefresh.setOnClickListener {
@@ -38,26 +35,35 @@ class MainActivity : AppCompatActivity() {
 
     }
     //Showing actual lap
-    fun sumLap(){
-        if (this.actualLaps == (this.lapsNum - 1)){
-            //When actual lap and laps number entered are the same
-            this.prizeImage.setImageResource(R.drawable.trofeo) //show first place price
-            actualLaps++ //Add one lap on the text view
-            //Showing complete laps text
-            Toast.makeText(this@Running, "Terminaste de correr!", Toast.LENGTH_SHORT).show()
-            //
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)   //Starting next activity
+    private fun sumLap(){
+        //Only sum lap if there are less than 20 actual laps
+        if (actualLaps < 20){
+            if (this.actualLaps == (this.maxLaps - 1)){
+                actualLaps++ //Add one lap on the text view
+                //Showing complete laps text
+                Toast.makeText(this@MainActivity, "Terminaste de correr!", Toast.LENGTH_SHORT).show()
+            } else {
+                this.actualLaps++ //Add one lap on the text view
+                //Showing lacking laps
+                Toast.makeText(this@MainActivity, """Faltan ${maxLaps - actualLaps} vueltas""", Toast.LENGTH_SHORT).show()
+            }
+            textLaps.text = actualLaps.toString() //Showing actual lap
         } else {
-            this.actualLaps++ //Add one lap on the text view
-            //Showing lacking laps
-            Toast.makeText(this@Running, """Faltan ${lapsNum - actualLaps} vueltas""", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this@MainActivity, "Llegaste al maximo", Toast.LENGTH_SHORT).show()
+        }
+    }
+    //Showing prize
+    private fun showPrize(){
+        if (actualLaps == 10) {
+            this.prizeImage.setImageResource(R.drawable.second) //show first place price
+        } else if (actualLaps == 20) {
+            this.prizeImage.setImageResource(R.drawable.first) //show first place price
         }
     }
     //Refreshing progress
-    fun refreshLaps(){
+    private fun refreshLaps(){
         actualLaps = 0
         textLaps.text = actualLaps.toString()
-        this.prizeImage.setImageResource(R.drawable.first) //show empty image
+        this.prizeImage.setImageResource(R.drawable.noback) //show empty image
     }
 }
